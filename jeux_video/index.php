a7c6c03aa6153685d597063ccf4e4feb2176b815
peyau <jonathan.levaux.7@hotmail.com>
2019-04-01 18:31:19 +0200

    Pagination

--- jeux_video/index.php
+++ jeux_video/index.php
@@ -1,25 +1,49 @@
-<head>
-  <meta charset="UTF-8" />
-</head>
-
 <?php
-// Connexion à la base de données
-$bdd = new PDO ('mysql:host=localhost;dbname=test2', 'root', '');
+// connexion � la base de donn�es
+include('connect.php');
 
-// Préparation de la requêtemp
-$req = $bdd->prepare('SELECT * FROM jeux_video');
+// r�cup�ration des limites d'affichage des donn�es
+if (isset($_GET['debut'])) {
+	$debut=(int)($_GET['debut']);
+} else {
+	$debut=0;
+}
+$nombre=10; // c'est le nombre d'enregistrements affich�s sur une page
+
+// calcul du nombre de pages
+$req = $bdd->query('SELECT COUNT(*) FROM jeux_video');
+$rep = $req->fetch();
+$nbenreg = $rep[0]; // r�cup�ration du nombre d'enregistrements pr�sents dans la table
+$req->closeCursor();
+
+//Le nombre de pages est �gal au nombre d'enregistrements / nb enreg par page
+$nbpages=ceil($nbenreg/$nombre); // ceil permet d'arrondir le nombre au nombre supérieur
+
+// preparation de la requ�te
+$req = $bdd->prepare('SELECT * FROM jeux_video LIMIT :debut, :nombre') or die(print_r($bdd->errorInfo()));
+$req->bindParam('debut', $debut, PDO::PARAM_INT);
+$req->bindParam('nombre', $nombre, PDO::PARAM_INT);
 $req->execute();
 
 echo '<table border="1">';
 echo '<tr><td>Nom</td><td>Possesseur</td><td>Console</td><td>Prix</td><td>Nb de joueurs max.</td><td>Commentaire</td></tr>';
+while ($donnees = $req->fetch())
+{
+	echo '<tr><td>'.$donnees['nom'].'</td>';
+	echo '<td>'.$donnees['possesseur'].'</td>';
+	echo '<td>'.$donnees['console'].'</td>';
+	echo '<td>'.$donnees['prix'].'</td>';
+	echo '<td>'.$donnees['nbre_joueurs_max'].'</td>';
+	echo '<td>'.$donnees['commentaires'].'</td></tr>';
+}
+echo '</table><br />-';
 
-while ($donnees = $req->fetch()) {
-  echo '<tr><td>' . $donnees['nom'] . '</td>';
-  echo '<td>' . $donnees['possesseur'] . '</td>';
-  echo '<td>' . $donnees['console'] . '</td>';
-  echo '<td>' . $donnees['prix'] . '</td>';
-  echo '<td>' . $donnees['nbre_joueurs_max'] . '</td>';
-  echo '<td>' . $donnees['commentaires'] . '</td></tr>';
+for ($i=1 ; $i<=$nbpages ; $i++)
+{
+ echo ' <a href="index.php?debut=' . ($i-1)*$nombre .'">' .$i . '</a> -';
+
 }
 
-echo '</table>';
+$req->closeCursor();
+
+?>
--- premiere_application/connect.php
+++ premiere_application/connect.php
@@ -5,4 +5,8 @@ $dbname='premiere_appli';
 $username='root';
 $password='';
 
+try {
 $bdd = new PDO ('mysql:host='.$hostname.';dbname=' . $dbname, $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+} catch (\Exception $e) {
+    echo 'Erreur de connexion à la base de données : ', $e->getMessage();
+}
