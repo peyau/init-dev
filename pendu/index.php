1de778a6f0d3dd8f8abf765387ecd6b437151bc2
peyau <jonathan.levaux.7@hotmail.com>
2019-04-24 20:00:29 +0200

    Modification de index.php

--- pendu/index.php
+++ pendu/index.php
@@ -1,70 +1,82 @@
 <?php
 session_start();
-const MAX_ESSAIS=10;
+const MAX_ESSAIS = 10;
+$alphabet = range('a', 'z');
 
 $_SESSION['essais'] = isset($_SESSION['essais']) ? $_SESSION['essais'] : 0;
 //$_SESSION['essais'] = $_SESSION['essais'] ?? 0;
-
-$mots = [
-    'coquelicot',
-    'cinema',
-    'patate',
-    'reminiscence',
-    'chat',
-    'doubleur',
-    'chinoises',
-    'bijouterie'
+/*unset($_SESSION);
+session_destroy();
+exit;*/
+$mots  = [
+  "coquelicot",
+  "cinema",
+  "patate",
+  "reminiscence",
+  "chat",
+  "doubleur",
+  "chinoises",
+  "bijouterie"
 ];
+if($_SESSION['essais'] === 0) {
+/*  if(isset($_SESSION['mot'])){
+    header('refresh: 5; url= index.php');
+    echo "vous avez perdu";
+    exit;
+  }*/
+  $position = rand(0, count($mots)-1);
+  $_SESSION['mot'] = $mots[$position];
+  $explodeWord = str_split($_SESSION['mot']);
+  $_SESSION['lettres_mot'] = array_unique($explodeWord);
+  $_SESSION['essais'] = MAX_ESSAIS;
+  $_SESSION['alphabet'] = range('a', 'z');
+  $_SESSION['lettres_utilisees'] = [];
+} else if(isset($_POST['lettre'])) {
 
-$lettres=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'x', 'z'];
-
-
-
-if ($_SESSION['essais']==0){
-    $nbAleatoire=rand(0, count($mots)-1);
-    $_SESSION['mot']=$mots[$nbAleatoire];
-    echo 'test';
-    $_SESSION['lettreMot']=array_unique(str_split($_SESSION['mot'])); // Explode un mot et supprime les doublons du tableau
-
-    $_SESSION['essais']=MAX_ESSAIS;
-
-    $_SESSION['lettresUtilisees']=[]; // -> Lettre utilisée dans un array_push + vérif sur le tableau après?
-} elseif(isset($_POST['lettre'])) {
-    if (in_array($_POST['lettre'], $_SESSION['lettresUtilisees'])) { // Si la lettre existe dans le tableau
-        echo 'La lettre a déjà été utilisée<br>'; // On affiche qu'elle existe déjà
+  //stocker la lettre si elle n'existe pas dans lettres utilisées
+  if(!in_array($_POST['lettre'], $alphabet)){
+      echo 'Ce n\'est pas une lettre de l\'alphabet';
+  }
+  else if(in_array($_POST['lettre'], $_SESSION['lettres_utilisees'])) {
+    echo  "Vous avez déjà utilisé cette lettre";
+  } else {
+    $_SESSION['lettres_utilisees'][] = $_POST['lettre'];
+    $index = array_search($_POST['lettre'], $_SESSION['alphabet']);
+    unset($_SESSION['alphabet'][$index]);
+    if(strpos($_SESSION['mot'], $_POST['lettre']) !== false) {
+      echo 'oui';
     } else {
-        array_push($_SESSION['lettresUtilisees'], $_POST['lettre']); // Sinon on la met dedans
+      $_SESSION['essais']--;
+      echo 'non';
     }
-    //echo $_SESSION['mot'] . '<br>';
-    if (strstr($_SESSION['mot'], $_POST['lettre'])) { // Si la lettre existe dans le mot
-        echo 'oui';
-    } else {
-        echo 'non';
-    }
+  }
 }
-
-//echo '<pre>';
-//print_r($_SESSION['lettreMot']);
-//echo str_replace($lettres, '*', $_SESSION['mot']); // cache les lettres du mot
-
-
 ?>
+
+<br>
+<div> Il vous reste <strong><?= $_SESSION['essais'] ?></strong> essais </div>
+
 
 <form action="" method="post">
-
 <?php
-$detail = [];
-for ($i=0; $i < strlen($_SESSION['mot']); $i++) {
-$detail[] = '_';
-}
-echo implode($detail, ' ');
-?>
-
-<div>
-    <label>Lettre </label>
+echo str_replace($_SESSION['alphabet'], '_', $_SESSION['mot']);
+/*
+  $detail = [];
+  for($i=0; $i<strlen($_SESSION['mot']); $i++) {
+    if(in_array($_SESSION['mot'][$i], $_SESSION['lettres_utilisees'])){
+      $detail[] = $_SESSION['mot'][$i];
+    } else {
+      $detail[] = '_';
+    }
+  }
+  echo implode($detail, ' ');
+  */
+ ?>
+  <div>
+    <label>Lettre</label>
     <input maxlength="1" type="text" name="lettre" value="">
-</div>
-<div>
+  </div>
+  <div>
     <input type="submit" value="Envoyer">
-</div>
+  </div>
 </form>
